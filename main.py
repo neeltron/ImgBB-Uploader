e8b0130ca4cafc97f9d3b648cb47fbf709a3d34b
Neel Adwani <nprosofficial@gmail.com>
2021-07-02 13:18:03 +0000

    Input + API Integration

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import requests
+import base64
 
-url = "https://api.imgbb.com/1/upload"
-payload = {
-"key": "lalalala",
-"image": base64.b64encode(file.read()),
-}
-res = requests.post(url, payload)
-dict = res.json()
-url = dict['data']['url']
-print(url)
+image = input("Enter the path to your image: ")
+
+with open(image, "rb") as file:
+  url = "https://api.imgbb.com/1/upload"
+  payload = {
+  "key": "lalalala",
+  "image": base64.b64encode(file.read()),
+  }
+  res = requests.post(url, payload)
+  dict = res.json()
+  url = dict['data']['url']
+  print("The URL of your image is: ", url)
