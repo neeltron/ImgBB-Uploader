791392e7cffcc73551a3d163a13110fef6f3127c
Neel Adwani <nprosofficial@gmail.com>
2021-07-02 13:19:32 +0000

    Added API key as a secret

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import requests
 import base64
+import os
 
 image = input("Enter the path to your image: ")
 
 with open(image, "rb") as file:
   url = "https://api.imgbb.com/1/upload"
   payload = {
-  "key": "lalalala",
+  "key": os.environ['imgbb'],
   "image": base64.b64encode(file.read()),
   }
   res = requests.post(url, payload)
